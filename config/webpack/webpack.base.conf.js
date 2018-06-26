fb4015f3b1cb4755801f3761a5a5afa801a985b9
qiwentao <qiwentao@MacdeiMac.local>
2018-06-26 15:34:47 +0800

    build: webpack热更新 api转发

---
+++ config/webpack/webpack.base.conf.js
@@ -0,0 +1,10 @@
+let config = {
+  entry: {
+    'index': './entryBuild/index.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.css', '.pcss']
+  }
+};
+
+module.exports = config;
---
+++ config/webpack/webpack.dev.conf.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack');
+const opn = require('opn'); // 用于打开浏览器
+const merge = require('webpack-merge');
+const path = require('path');
+const baseWebpackConfig = require('./webpack.base.conf');
+const webpackFile = require('./webpack.file.conf');
+
+let config = merge(baseWebpackConfig, {
+  mode: 'development',
+  output: {
+    path: path.resolve(webpackFile.devDirectory),
+    filename: 'js/[name].js',
+    chunkFilename: 'js/[name].js',
+    publicPath: '',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: ['babel-loader'],
+        include: [
+          path.resolve(__dirname, '../../app'),
+          path.resolve(__dirname, '../../entryBuild'),
+        ],
+        exclude: [
+          path.resolve(__dirname, '../../node_modules'),
+        ],
+      },
+    ]
+  },
+  devServer: {
+    host: '0.0.0.0',
+    port: 9090,
+    hot: true,
+    inline: true,
+    contentBase: path.resolve(webpackFile.devDirectory),
+    historyApiFallback: true,
+    disableHostCheck: true,
+    proxy: [
+      {
+        context: ['/api/**', '/u/**'],
+        target: 'http://192.168.12.100:9090/',
+        secure: false,
+      },
+    ],
+    after() {
+      opn('http://localhost:' + this.port)
+    },
+  },
+});
+
+module.exports = config;
