09d73bf99dbf61cf8d00a1852eeac92f6e86afac
qiwentao <qiwentao@MacdeiMac.local>
2018-06-26 17:42:54 +0800

    build: postcss 和 图片字体webpack配置

--- app/component/index/Index.jsx
+++ app/component/index/Index.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import '../../public/css/index.pcss';
 
 export default class Index extends Component {
     render() {
         return (
-            <div>
-            这是首页ma 
+            <div className="cont">
+                <div className="top">
+                    <div>头部</div>
+                    <i className="logo" />
+                </div>
+                <div className="bottom">底部</div>
             </div>
         );
     }
